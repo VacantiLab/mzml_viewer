1a9458f070f8465df2e79ca26402c7c492235a1d
Nathaniel Vacanti <nate@Nathaniels-iMac.local>
2020-07-31 17:33:10 -0400

    made mzml_bokeh work without prior knowledge of mzml file

--- mzml_bokeh.py
+++ mzml_bokeh.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pyteomics import mzml, auxiliary
-import pdb
+from pdb import set_trace
 from copy import copy
 
 from bokeh.io import curdoc
@@ -10,51 +10,85 @@ from bokeh.plotting import figure
 from bokeh.events import DoubleTap
 
 # bokeh serve --show mzml_bokeh.py
+mzml_file_directory = '/Users/nate/Dropbox/Research/Vacanti_Laboratory/mzml_files/QE1_QC_HeLa_20200225_r2.mzML'
 
-MZML = mzml.read('/Users/nate/Dropbox/Research/Vacanti_Laboratory/mzml_files/20200226_1522_HeLa_NoFilters.mzML',dtype=dict)
+MZML = mzml.read(mzml_file_directory,dtype=dict)
+n_intensities_array = np.array([])
+i=0
+for key in MZML:
+    n_intensities_key = len(np.array(key['m/z array'],dtype='float'))
+    n_intensities_array = np.append(n_intensities_array,n_intensities_key)
+    i = i+1
+    print(i)
+most_intensities = int(max(n_intensities_array))
 
-tic = np.zeros(18440)
-time = np.zeros(18440)
-n_intensities_rt = np.zeros(18440)
-most_intensities_rt = 28463
-
+MZML = mzml.read(mzml_file_directory,dtype=dict)
+time = np.array([])
+tic = np.array([])
 ic_mz_dict = {}
 i = 0
 for key in MZML:
-    time[i] = float(key['scanList']['scan'][0]['scan start time'])
-    #mzs = np.round(key['m/z array'],decimals=4)
-    #mz_indices = np.isin(xvalues,mzs)
-    #intensities = np.zeros(n_mzs)
-    #intensities[mz_indices] = key['intensity array']
-    #if i==0:
-    #    MyDict['y'] = copy(intensities[mz_indices])
+    time_point = float(key['scanList']['scan'][0]['scan start time'])
+    time = np.append(time,time_point)
     xarray = np.array(key['m/z array'],dtype='float') #these must be redefined as type 'float' from 'object' so that the 'maximum' attribute works in np.pad
     yarray = np.array(key['intensity array'],dtype='float') #these must be redefined as type 'float' from 'object' so that the 'maximum' attribute works in np.pad
-
-    n_zeros = most_intensities_rt - len(xarray)
-
+    n_zeros = most_intensities - len(xarray)
     xarray = np.pad(xarray,[0,n_zeros],'maximum')
     yarray = np.pad(yarray,[0,n_zeros],'constant')
-
 
     if i==0:
         ic_mz_plot_dict = {}
         ic_mz_plot_dict['x'] = xarray
         ic_mz_plot_dict['y'] = yarray
 
-    #MyDict seems to get too big and the function ColumnDataSource seems to time out
-    #    the nunber of scans seems to be the problem, but the number of mz values may also be problematic
-
     ic_mz_dict['x'+'%.3f'%(time[i])] = xarray
     ic_mz_dict['y'+'%.3f'%(time[i])] = yarray
-    n_intensities_rt[i] = len(ic_mz_dict['y'+'%.3f'%(time[i])])
 
-    tic[i] = sum(key['intensity array'])
+    tic = np.append(tic,sum(key['intensity array']))
+
     i = i+1
     print(i)
 
-n_keys = i
-#most_intensities = max(n_intensities)
+# tic = np.zeros(n_keys)
+# time = np.zeros(n_keys)
+
+# MZML = mzml.read('/Users/nate/Dropbox/Research/Vacanti_Laboratory/mzml_files/20200226_1522_HeLa_NoFilters.mzML',dtype=dict)
+# ic_mz_dict = {}
+# i = 0
+# for key in MZML:
+#     time[i] = float(key['scanList']['scan'][0]['scan start time'])
+#     #mzs = np.round(key['m/z array'],decimals=4)
+#     #mz_indices = np.isin(xvalues,mzs)
+#     #intensities = np.zeros(n_mzs)
+#     #intensities[mz_indices] = key['intensity array']
+#     #if i==0:
+#     #    MyDict['y'] = copy(intensities[mz_indices])
+#     xarray = np.array(key['m/z array'],dtype='float') #these must be redefined as type 'float' from 'object' so that the 'maximum' attribute works in np.pad
+#     yarray = np.array(key['intensity array'],dtype='float') #these must be redefined as type 'float' from 'object' so that the 'maximum' attribute works in np.pad
+#
+#     n_zeros = most_intensities - len(xarray)
+#
+#     xarray = np.pad(xarray,[0,n_zeros],'maximum')
+#     yarray = np.pad(yarray,[0,n_zeros],'constant')
+#
+#
+#     if i==0:
+#         ic_mz_plot_dict = {}
+#         ic_mz_plot_dict['x'] = xarray
+#         ic_mz_plot_dict['y'] = yarray
+#
+#     #MyDict seems to get too big and the function ColumnDataSource seems to time out
+#     #    the nunber of scans seems to be the problem, but the number of mz values may also be problematic
+#
+#     ic_mz_dict['x'+'%.3f'%(time[i])] = xarray
+#     ic_mz_dict['y'+'%.3f'%(time[i])] = yarray
+#
+#     tic[i] = sum(key['intensity array'])
+#     i = i+1
+#     print(i)
+#
+# n_keys = i
+# #most_intensities = max(n_intensities)
 
 tic_dict = {'x':time, 'y':tic}
 tic_source = ColumnDataSource(data=tic_dict)
